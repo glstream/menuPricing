e0134fcbf7f0655c318333ddb682d5c2385dbb18
glstream <grayson.stream@gmail.com>
2017-06-13 19:51:16 -0700

    More pricing

--- menu/latte.js
+++ menu/latte.js
@@ -1,11 +1,17 @@
 var latte = (temperature, callback) => {
-   if (temperature >= 70) {
+   if (temperature <= 70) {
        price = 5.11
+   } else if (temperature <= 50){
+       price = 5.16
+   } else if (temperature <= 40){
+       price = 5.21
+   } else if (temperature <= 30){
+       price = 5.24
+   } else if (temperature <= 20){
+       price = 6.00
    } else {
        price = 4.95
    }
-
-
    callback(undefined, {
                 price: price
             })  
