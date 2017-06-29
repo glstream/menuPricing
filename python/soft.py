33f2d3e949ff9bb3f7b19d1691cf58406e04e8b2
glstream <grayson.stream@gmail.com>
2017-06-28 21:10:52 -0700

    papirus addtion

--- python/soft.py
+++ python/soft.py
@@ -5,6 +5,18 @@ from PIL import ImageFont, ImageDraw, Image
 import sys
 import os
 import time
+from pymongo import MongoClient
+
+connection = MongoClient('ds139262.mlab.com', 39262)
+db = connection['category']
+db.authenticate('glstream', 'Stream100')
+
+menu = db.menu
+
+
+product = menu.find_one({'product': 'Latte'})
+
+print(product['price'])
 
 # Check EPD_SIZE is defined
 EPD_SIZE=0.0
